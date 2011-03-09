ca11ff5ea91e92774ed84875d8fdf90832e5569b
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-03-10 00:04:35 +0900

    １個ずつ最新画像を取得

--- app.rb
+++ app.rb
@@ -129,7 +129,8 @@ post '/subscription/callback' do
   data.each do |obj|
     data = JSON::parse(REDIS.get("subscription:#{obj['object_id']}"))
     max_timestamp = REDIS.get("subscription:#{obj['object_id']}:max_timestamp")
-    opt = {:distance => data['radius'], :count => 5, :min_timestamp => max_timestamp}
+    # opt = {:distance => data['radius'], :count => 5, :min_timestamp => max_timestamp}
+    opt = {:distance => data['radius'], :count => 1, :min_timestamp => max_timestamp}
     images = Instagram.media_search(data['lat'], data['lng'], opt)
     push_data = []
     images.each do |image|
